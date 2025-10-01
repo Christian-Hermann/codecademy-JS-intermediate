ddb7c75d6d3ec9fc7a07b2f4e32b0d6fc3fe9b6c
Your NameChristian <christianhermanndev@gmail.com>
2025-10-01 12:38:38 -0500

    Before methods in #6

---
+++ Classes/Build a Library.js
@@ -0,0 +1,28 @@
+class Media {
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  get title() {
+    return this._title;
+  }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
+  get ratings() {
+    return this._ratings;
+  }
+
+  set isCheckedOut(value) {
+    this._isCheckedOut = value;
+  }
+  toggleCheckOutStatus() {
+    this._isCheckedOut = !this._isCheckedOut;
+  }
+  getAverageRating() {
+    let ratingsSum = this.ratings.reduce((acc, curr) => acc + curr, 0);
+    return ratingsSum / this.ratings.length;
+  }
+}
