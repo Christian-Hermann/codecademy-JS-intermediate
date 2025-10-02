c2a3f974a057cd9a4cbee7ab8619397666b11fb3
Your NameChristian <christianhermanndev@gmail.com>
2025-10-02 18:25:06 -0500

    Finished Message Mixer

--- Classes/School Catalogue.js
+++ Classes/School Catalogue.js
@@ -37,4 +37,43 @@ class PrimarySchool extends School {
     super(name, "primary", numberOfStudents);
     this._pickupPolicy = pickupPolicy;
   }
+  get pickupPolicy() {
+    return this._pickupPolicy;
+  }
 }
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, "high", numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+  get sportsTeams() {
+    console.log(this._sportsTeams);
+  }
+}
+
+const lorraineHansbury = new PrimarySchool(
+  "LorraineHansbury",
+  514,
+  "Students must be picked up by a parent, guardian, or a family member over the age of 13."
+);
+
+lorraineHansbury.quickFacts();
+
+School.pickSubstituteTeacher([
+  "Jamal Crawford",
+  "Lou Williams",
+  "J. R. Smith",
+  "James Harden",
+  "Jason Terry",
+  "Manu Ginobli",
+]);
+
+const alSmith = new HighSchool("Al E. Smith", 415, [
+  "Baseball",
+  "Basketball",
+  "Volleyball",
+  "Track and Field",
+]);
+
+alSmith.sportsTeams;
