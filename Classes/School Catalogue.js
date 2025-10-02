a8cc2908a30bc99a7ed80821aae2688294a2e6ad
Your NameChristian <christianhermanndev@gmail.com>
2025-10-02 13:23:11 -0500

    Through #11

---
+++ Classes/School Catalogue.js
@@ -0,0 +1,40 @@
+class School {
+  constructor(name, level, numberOfStudents) {
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+  get name() {
+    return this._name;
+  }
+  get level() {
+    return this._level;
+  }
+  get numberOfStudents() {
+    return this._numberOfStudents;
+  }
+  set numberOfStudents(value) {
+    if (typeof value === "number") {
+      this._numberOfStudents = value;
+    } else {
+      console.log("Invalid input: numberOfStudents must be set to a Number.");
+    }
+  }
+  quickFacts() {
+    console.log(
+      `${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`
+    );
+  }
+  static pickSubstituteTeacher(substituteTeachers) {
+    return substituteTeachers[
+      Math.floor(Math.random() * substituteTeachers.length)
+    ];
+  }
+}
+
+class PrimarySchool extends School {
+  constructor(name, numberOfStudents, pickupPolicy) {
+    super(name, "primary", numberOfStudents);
+    this._pickupPolicy = pickupPolicy;
+  }
+}
