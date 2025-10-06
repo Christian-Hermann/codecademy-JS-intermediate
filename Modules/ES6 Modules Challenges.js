2e9565b862da38642322a9d6ca9123b4320989bc
Your NameChristian <christianhermanndev@gmail.com>
2025-10-06 14:48:55 -0500

    Start WorkAround Explorer

--- Modules/ES6 Modules Challenges.js
+++ Modules/ES6 Modules Challenges.js
@@ -1,4 +1,4 @@
-// #2
+// Challenege #2
 
 /* main.js */
 
@@ -12,10 +12,52 @@ setInterval(() => {
   // call changeToFunkyColor() here
 }, 200);
 
-// #3
+// Challenge #3
 // The <script> element below loads a JavaScript file, which makes use of ES6 import/export syntax. Which of the following changes must be made in order to properly load this module and avoid errors in the browser console?
 
 <script src="./my-module.js"></script>;
 
 // answer
 <script type="module" src="./my-module.js"></script>;
+
+// Challenge #4
+// Consider the following functions being exported from the modules square-area.js and
+// circle-area.js:
+
+/* From square-area.js... */
+export function area(side) {
+  return side * side;
+}
+
+/* From circle-area.js... */
+export function area(radius) {
+  return Math.PI * radius * radius;
+}
+
+// answer
+import { area as squareArea } from "square-area.js";
+import { area as circleArea } from "circle-area.js";
+
+console.log("The area of a square with sides of length 5 is " + squareArea(5));
+console.log("The area of a circle with radius of length 5 is " + circleArea(5));
+
+// Challenge #5
+function changeText(domElement, newText) {
+  domElement.innerHTML = newText;
+}
+
+function changeToFunkyColor(domElement) {
+  const r = Math.random() * 255;
+  const g = Math.random() * 255;
+  const b = Math.random() * 255;
+
+  domElement.style.color = `rgb(${r}, ${g}, ${b})`;
+}
+
+// use default export syntax below here
+export default {
+  changeText,
+  changeToFunkyColor,
+};
+
+// Challenge #6
