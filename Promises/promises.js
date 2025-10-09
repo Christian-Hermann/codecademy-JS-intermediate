51e9f862413528a955dce9fa60241638b2d5edc8
Your NameChristian <christianhermanndev@gmail.com>
2025-10-09 13:18:31 -0500

    Success and Failure Callback Functions

--- Promises/promises.js
+++ Promises/promises.js
@@ -23,3 +23,16 @@ const orderPromise = orderSunglasses();
 console.log(orderPromise);
 
 // The Node is setTimeout() Function
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+const usingSTO = () => {
+  console.log("Not enough time in the day!");
+};
+
+setTimeout(usingSTO, 2000);
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
+
+// Success and Failure Callback Functions
