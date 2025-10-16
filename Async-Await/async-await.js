5a267f73155deadd2080073db4d0ee7c6f6b7cdf
Your NameChristian <christianhermanndev@gmail.com>
2025-10-16 14:35:46 -0500

    Finished Hadnling Dependent Pomises

--- Async-Await/async-await.js
+++ Async-Await/async-await.js
@@ -16,3 +16,21 @@ withAsync(100).then((resolveValue) => {
 });
 
 // The await Operator
+// Native promise version:
+function nativePromiseDinner() {
+  brainstormDinner().then((meal) => {
+    console.log(`I'm going to make ${meal} for dinner.`);
+  });
+}
+nativePromiseDinner();
+
+// async/await version:
+async function announceDinner() {
+  // Write your code below:
+  let meal = await brainstormDinner();
+  console.log(`I'm going to make ${meal} for dinner.`);
+}
+
+announceDinner();
+
+// Handling Dependent Promises
