4ad4512fe346a7055cfd3e4951fc50095c52fec0
Your NameChristian <christianhermanndev@gmail.com>
2025-10-16 12:58:02 -0500

    The await Operator

---
+++ Async-Await/async-await.js
@@ -0,0 +1,16 @@
+// Tha async Keyword
+async function withAsync(value) {
+  if (value === 0) {
+    return "zero";
+  } else {
+    return "not zero";
+  }
+}
+
+// Leave this commented out until step 3:
+
+withAsync(100).then((resolveValue) => {
+  console.log(
+    ` withAsync(100) returned a promise which resolved to: ${resolveValue}.`
+  );
+});
