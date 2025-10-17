29830832f08e3502fd28afd9713865cce06d5282
Your NameChristian <christianhermanndev@gmail.com>
2025-10-17 12:24:13 -0500

    Finished Await Promise.all()

--- Async-Await/async-await.js
+++ Async-Await/async-await.js
@@ -75,3 +75,20 @@ async function serveDinner() {
 }
 
 serveDinner();
+
+// Await Promise.all()
+
+// Write your code below:
+async function serveDinnerAgain() {
+  let foodArray = await Promise.all([
+    steamBroccoli(),
+    cookRice(),
+    bakeChicken(),
+    cookBeans(),
+  ]);
+  console.log(
+    `Dinner is served. We're having ${foodArray[0]}, ${foodArray[1]}, ${foodArray[2]}, and ${foodArray[3]}.`
+  );
+}
+
+serveDinner;
