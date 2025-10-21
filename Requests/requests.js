10935b7e4697029bfb7241baf156692a69d978c5
Your NameChristian <christianhermanndev@gmail.com>
2025-10-21 15:44:41 -0500

    Starting Film Finder

--- Requests/requests.js
+++ Requests/requests.js
@@ -75,3 +75,18 @@ const shortenUrl = () => {
 };
 
 // Making an async GET request
+const getSuggestions = async () => {
+  const wordQuery = inputField.value;
+  const endpoint = url + queryParams + wordQuery;
+  try {
+    const response = await fetch(endpoint, { cache: "no-cache" });
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// Making an async POST request
