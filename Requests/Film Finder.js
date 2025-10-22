d917f7110ecfa56456f723221ec3c41627abe3fe
Your NameChristian <christianhermanndev@gmail.com>
2025-10-22 11:45:27 -0500

    Started async getGenres

---
+++ Requests/Film Finder.js
@@ -0,0 +1,9 @@
+const tmdbKey = "22cf13a639caaaf620c04b11fa44c208";
+const tmdbBaseUrl = "https://api.themoviedb.org/3";
+const playBtn = document.getElementById("playBtn");
+
+const getGenres = async () => {
+  const genreRequestEndpoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = tmdbBaseUrl + genreRequestEndpoint + requestParams;
+};
