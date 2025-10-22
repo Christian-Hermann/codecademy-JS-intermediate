1c68cf4f485d8e52edc3b64bf5e357e2fc408ec0
Your NameChristian <christianhermanndev@gmail.com>
2025-10-22 13:17:28 -0500

    Finished getMovies

--- Requests/Film Finder.js
+++ Requests/Film Finder.js
@@ -18,3 +18,20 @@ const getGenres = async () => {
     console.log(error);
   }
 };
+
+const getMovies = async () => {
+  const selectedGenre = getSelectedGenre();
+  const discoverMovieEndpoint = "/discover/movie";
+  const requestParams = `?api_key=${tmdbKey}&with_genres=${selectedGenre}`;
+  const urlToFetch = tmdbBaseUrl + discoverMovieEndpoint + requestParams;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      const movies = jsonResponse.results;
+      return movies;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
