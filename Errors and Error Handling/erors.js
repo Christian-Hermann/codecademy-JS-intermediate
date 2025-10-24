a1c04e348053dd279c865ded894df6ea045622df
Your NameChristian <christianhermanndev@gmail.com>
2025-10-24 12:46:14 -0500

    The try... catch statement

--- Errors and Error Handling/erors.js
+++ Errors and Error Handling/erors.js
@@ -44,3 +44,11 @@ function throwError() {
 throwError();
 
 console.log("Because of the error, this will not be printed!");
+
+// Constructing an Error
+// Write your code below:
+console.log(Error("User missing name"));
+
+console.log("Will logging the error stop our program from running?");
+
+// The try... catch Statement
